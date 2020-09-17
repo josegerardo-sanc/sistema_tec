a3bddb2a43765ead9ff227b55c0f150211acf714
josegerardo-sanc <sanchezalvaradojose0@gmail.com>
2020-09-17 18:50:40 -0500

    eliminacion del footer y cambio de idioma

--- seccionusuario/mapeosat.php
+++ seccionusuario/mapeosat.php
@@ -274,27 +274,7 @@ if (isset($_GET['sinoca'])) {
 							?>
 							
 								</tbody>
-							<tfoot>
-								<tr>
-								<th>Nomeclaturado</th>
-									<th>Ubicacion Switch</th>
-									<th>No. patch Panel</th>
-									<th>No. Puerto Patch Panel</th>
-									<th>No. Switch</th>
-									<th>Serie Switch</th>
-									<th>Puerto Switch</th>
-									<th>Tipo de Equipo</th>
-									<th>Administración</th>
-									<th>Área</th>
-									<th>Vlan</th>
-									<th>Actividad</th>
-									<th>Observaciones</th>
-									<?php //if ($tusuario == 1): ?>
-									<th>Situación Nodo</th>
-									<?php //endif ?>
-									<th>Opciones</th>
-								</tr>
-							</tfoot>
+						
 						</table>
 
 				     
@@ -486,6 +466,31 @@ include 'plantilla/pies.php';
 
  <script>
 	 $(document).ready(function(){
+		 
+		 
+		 var Idioma = {
+			"emptyTable": "<i>No hay datos disponibles en la tabla.</i>",
+			"info": "Del _START_ al _END_ de _TOTAL_ ",
+			"infoEmpty": "Mostrando 0 registros de un total de 0.",
+			"infoFiltered": "(filtrados de un total de _MAX_ registros)",
+			"infoPostFix": "(actualizados)",
+			"lengthMenu": "Mostrar _MENU_ registros",
+			"loadingRecords": "Cargando...",
+			"processing": "Procesando...",
+			"search": "<span style='font-size:15px;'>Buscar:</span>",
+			"searchPlaceholder": "Dato para buscar",
+			"zeroRecords": "No se han encontrado coincidencias.",
+			"paginate": {
+				"first": "Primera",
+				"last": "Última",
+				"next": "Siguiente",
+				"previous": "Anterior"
+			},
+			"aria": {
+				"sortAscending": "Ordenación ascendente",
+				"sortDescending": "Ordenación descendente"
+			}
+		}
 
 			
 		$('#call_modal_nuevo').on('click',function(){
@@ -499,7 +504,11 @@ include 'plantilla/pies.php';
 		})
 
 		var table = $('#example').DataTable( {
-			responsive: true
+			"responsive": true,
+			"destroy": true,
+			"language": Idioma,
+			"lengthMenu": [[10, 20, 25, 50, -1], [10, 20, 25, 50, "Todos"]],
+			"iDisplayLength": 10
 		});
 	
 		new $.fn.dataTable.FixedHeader( table );
